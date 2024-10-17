52b23c6594ba56360173d80d8a309919e18693cb
Shyamala21674 <thisshyamala@gmail.com>
2024-10-17 09:53:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+    <Welcome proname="Ram"/>
+      This is a App component
+      <Welcome proname="Sita"/>
+      <Welcome proname="Krish"/>
+      <Welcome proname="Ram"/>
+      <Welcome proname="Ram"/>
+      
+
+     
+      </div>
+  );
+}
+
+export default App;
---
+++ welcome.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import welcome from './'
+
+function welcome(){
+    return(
+      <div className='App'>
+        this is welcome component
+      </div>
+
+
+
+
+    );
+    export default App;
+    export default welcome;
+}
